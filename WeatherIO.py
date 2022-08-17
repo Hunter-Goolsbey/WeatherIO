4292ba3ae1312783b6560fb6ccd5dd0efa74826e
Hunter-Goolsbey <110963084+Hunter-Goolsbey@users.noreply.github.com>
2022-08-16 22:27:38 -0700

    Create WeatherIO.py

---
+++ WeatherIO.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+#handles get request calls to RestAPI
+def get_BuildA(url, auth):
+  r = requests.get(url, headers={'Token': auth})
+  print('Request Code: ' + str(r.status_code) + '\n\n')
+  q = r.json()
+  
+  records = (q['results'])
+  
+  print('Record count: ' + str(len(records)) + '\n\n')
+  
+  for i in range(len(records)):
+    print('Elevation: ', records[i]['elevation'])
+    print('Latitude: ', records[i]['latitude'])
+    print('Longitude: ', records[i]['longitude'])
+    print('Name: ', records[i]['name'])
+    
+#handles user input, defined parameters, and Oauth2 token
+auth = str(input('Enter your NOAA auth token: '))
+
+
+While True:
+  zp_code = input('Enter Zip Code: ')
+  url = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/stations?locationid=ZIP:' + zp_code
+  print(url)
+  get_BuildA(url, auth)
+  
